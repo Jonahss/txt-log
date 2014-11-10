9ef690788c96210bf0275959da880c6f12afcb06
Jonah Stiennon <jonahss@gmail.com>
2014-11-09 23:00:27 -0800

    before it was working up to creating the classificationData stack, now the reduce is in place and it works :D  Next up: object stream rather than text

--- classify.js
+++ classify.js
@@ -1,5 +1,6 @@
 var through = require('through2');
-var mapping = require('./simpleMapping.js')
+var mapping = require('./simpleMapping.js');
+var _ = require('underscore');
 
 var classifyStream = through.ctor(function(chunk, enc, cb) {
   var self = this;
@@ -11,10 +12,10 @@ var classifyStream = through.ctor(function(chunk, enc, cb) {
 
 
 function classify(mapping, entry, cb) {
-entry = entry.toString();
+entry = entry.toString().slice(0, -1); //remove trailing newline
 //console.log('classify', entry)
 
-  function recurse(mapping, entry, classes, cb) {
+  function recurse(mapping, entry, classificationData, cb) {
     entry = entry.toString();
 
     Object.keys(mapping).some( function (key) {
@@ -22,15 +23,19 @@ entry = entry.toString();
       var split = entry.split(clazz.separator);
       var first = split[0];
       if (first.match(clazz.regex)) {
-        // cleanse, remove the "children" and "parse". i guess thats serialize or something
-        classes.push(clazz);
+        var classificationDatum = {
+          name: key,
+          raw: first,
+          meta_data: clazz.parse(first)
+        }
+        classificationData.push(classificationDatum);
         //console.log('fisrt', first)
         var remainingEntry = split.slice(1).join(clazz.separator);
         if (clazz.children) {
-          recurse(clazz.children, remainingEntry, classes, cb);
+          recurse(clazz.children, remainingEntry, classificationData, cb);
         } else {
-          classes.push(remainingEntry);
-          cb(classes);
+          classificationData.push(remainingEntry);
+          cb(classificationData);
         }
         return true;
       }
@@ -38,10 +43,27 @@ entry = entry.toString();
 
   };
 
-  recurse(mapping, entry, [], function(classes) {
-    //console.log('done recursing. classes for this line: ', classes.length)
-    cb(JSON.stringify(classes)+'\n');
-  });
+  recurse(mapping, entry, [], exit);
+
+  function exit(classificationData) {
+    var classified = classificationData.reduce(function (a, b) {
+      //the remaining log entry, after prefixes, is the last entry and is a string
+      if (typeof b == "string") {
+        return {
+          classes: a.classes,
+          meta_data: a.meta_data,
+          data: b
+        }
+      }
+
+      return {
+        classes: a.classes.concat(b.name),
+        meta_data: _.extend(a.meta_data,b.meta_data)
+      }
+    }, {classes: [], meta_data: {}});
+
+    cb(JSON.stringify(classified)+'\n');
+  }
 
 }
 
